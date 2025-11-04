9be1ddeded653ada99ad0d138613c2ab15ba20ac
eugenekormin <evgenykormin86@gmail.com>
2025-11-04 15:28:05 +1000

    frontend added

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+
+const Page = () => {
+    const smth = false;
+
+    return (
+        <div className="min-h-screen min-w-screen flex items-center justify-center">
+            <Button>
+                Click Me!
+            </Button>
+        </div>
+    );
+};
+
+export default Page;
